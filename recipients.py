1639a569cd90d274e1f68848285dc4ad89f0e45e
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-03-22 14:31:23 +0300

    Manually prompt user for recipient data

---
+++ recipients.py
@@ -0,0 +1,31 @@
+def get_recipients(cc=False, bcc=False):
+    recipients = {}
+
+    print("[Primary] recipients")
+    recipients["primary"] = get_addresses()
+
+    if cc:
+        print("\n[Cc] recipients")
+        recipients["cc"] = get_addresses()
+    if bcc:
+        print("\n[Bcc] recipients")
+        recipients["bcc"] = get_addresses()
+
+    return recipients
+
+
+def get_addresses():
+    addresses = []
+
+    while True:
+        address = input("Enter email address: ").strip()
+        if address == "":
+            break
+        addresses.append(address)
+
+    return addresses
+
+
+recipients = get_recipients(cc=True)
+
+print(recipients)
