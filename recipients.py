6c349cd238d51aa0b9c5b888ea6fd5a6bab41130
Kelvin Ziro <kelvin.ziroh@gmail.com>
2026-04-16 10:39:49 +0300

    Remove cc and bcc parameters in get_recipients func

--- recipients.py
+++ recipients.py
@@ -3,18 +3,17 @@ import sys
 from pathlib import Path
 
 
-def get_recipients(cc=False, bcc=False):
+def get_recipients():
     recipients = {}
 
     print("[Primary] recipients")
     recipients["primary"] = get_addresses()
 
-    if cc:
-        print("\n[Cc] recipients")
-        recipients["cc"] = get_addresses()
-    if bcc:
-        print("\n[Bcc] recipients")
-        recipients["bcc"] = get_addresses()
+    print("\n[Cc] recipients")
+    recipients["cc"] = get_addresses()
+
+    print("\n[Bcc] recipients")
+    recipients["bcc"] = get_addresses()
 
     return recipients
 
